701d8313abd6fb8bda490dba74a91b8c35bfef09
Prouser123 <jcahill773@gmail.com>
2018-11-26 20:20:11 +0000

    Add documentation

--- src/java/me/prouser123/kodicore/Utils.java
+++ src/java/me/prouser123/kodicore/Utils.java
@@ -17,12 +17,20 @@ public class Utils {
 	
 	public static class Player {
 		
-		// Return online players (List of usernames)
+		/**
+		 * Return online players (List of usernames)
+		 * 
+		 * @return Array of online player names
+		 */
 		public static Collection<? extends org.bukkit.entity.Player> getOnlinePlayersByNames() {
 			return Main.inst().getServer().getOnlinePlayers();
 		}
 		
-		// Return online players (amount of players as an int)
+		/**
+		 * Return online players (amount of players as an int)
+		 * 
+		 * @return Int amount of online players
+		 */
 		public static int getOnlinePlayersByAmount() {
 			return Main.inst().getServer().getOnlinePlayers().size();
 		}
@@ -31,20 +39,37 @@ public class Utils {
 	
 	public static class Broadcast {
 		
-		// Broadcast to permission (message, permission node)
-		public static int toPermission(String message, String permission) {
-			return Main.inst().getServer().broadcast(message, permission);
+		/**
+		 * Broadcast to permission (message, permission node) 
+		 * 
+		 * @param message Message to send
+		 * @param permission Permission node
+		 */
+		public static void toPermission(String message, String permission) {
+			Main.inst().getServer().broadcast(message, permission);
 		}
 		
-		// Broadcast to server (message)
-		public static int global (String message) {
-			return Main.inst().getServer().broadcastMessage(message);
+		/**
+		 * Broadcast to server (message)
+		 * 
+		 * @param message Message to send
+		 */
+		public static void global (String message) {
+			Main.inst().getServer().broadcastMessage(message);
 		}
 	}
 	
 	
 	public static class Config {
 		
+		/**
+		 * Get a String from getConfig() (plugin config file) using path and storing pluginName for logging to the console
+		 * 
+		 * @param path String path in config to value
+		 * @param getConfig getConfig() instance
+		 * @param pluginName Name of plugin for logging
+		 * @return String value from config
+		 */
 		// Get String
 		public static String getString(String path, FileConfiguration getConfig, String pluginName) {
 			// Store String
@@ -57,7 +82,14 @@ public class Utils {
 			return destination;
 		}
 		
-		// Get Int
+		/**
+		 * Get an Int from getConfig() (plugin config file) using path and storing pluginName for logging to the console
+		 * 
+		 * @param path Int path in config to value
+		 * @param getConfig getConfig() instance
+		 * @param pluginName Name of plugin for logging
+		 * @return Int value from config
+		 */// Get Int
 		public static int getInt(String path, FileConfiguration getConfig, String pluginName) {
 			// Store Int
 			int destination = getConfig.getInt(path);
@@ -71,12 +103,29 @@ public class Utils {
 	}
 	
 	
-	// Get World (by name)
+	/**
+	 * Get World (by name)
+	 * @param world String
+	 * @return world World
+	 * @see org.bukkit.World
+	 */
 	public static World getWorld(String world){
 		return Main.inst().getServer().getWorld(world);
 	}
 	
-	// Add item to inventory (using arguments)
+	/**
+	 * Add item to inventory (using arguments)
+	 * 
+	 * @param inv Bukkit Inventory
+	 * @see org.bukkit.inventory.Inventory
+	 * 
+	 * @param material Material
+	 * @see org.bukkit.Material
+	 * 
+	 * @param displayName Item Name
+	 * @param lore Item Lore
+	 * @param position Item Position
+	 */
 		public static void addInventoryItem(Inventory inv, Material material, String displayName, String lore, int position) {
 			// Create the ItemStack
 			ItemStack stack = new ItemStack(material);
